975d41df5bf715bbc90a8e79c8a12d431f978f17
Linus Dietz <linus.dietz@kcl.ac.uk>
2025-09-13 20:14:19 +0100

    small changes in hayagriva

--- src/main/java/org/jabref/logic/layout/format/HayagrivaType.java
+++ src/main/java/org/jabref/logic/layout/format/HayagrivaType.java
@@ -8,7 +8,7 @@ public class HayagrivaType implements LayoutFormatter {
     @Override
     public String format(String value) {
         return switch (StandardEntryType.valueOf(value)) {
-            case Article, Conference -> "article";
+            case Article, Conference, InCollection -> "article";
             case Book -> "book";
             case InBook -> "chapter";
             case Report, TechReport -> "report";
